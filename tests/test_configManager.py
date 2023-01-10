88e4da614931e6604577095fbf0c95c07836a733
flecmart <flecmart@gmail.com>
2023-01-10 21:16:46 +0100

    add more configManager tests

--- tests/test_configManager.py
+++ tests/test_configManager.py
@@ -6,13 +6,29 @@ sys.path.insert(0, os.path.abspath('.'))
 from app.configManager import ConfigManager, ConfigValidationError
 
 schema = 'app/config.schema.yaml'
-config_root_folder = 'tests/test_configs'
+test_config_root_folder = 'tests/test_configs'
 
 def test_example_config():
     cm = ConfigManager(schema, 'app/config.example.yaml')
 
 def test_valid_full_config():
-    cm = ConfigManager(schema, f'{config_root_folder}/valid.full.yaml')
+    cm = ConfigManager(schema, f'{test_config_root_folder}/valid.full.yaml')
     
 def test_valid_min_config():
-    cm = ConfigManager(schema, f'{config_root_folder}/valid.full.yaml')
+    cm = ConfigManager(schema, f'{test_config_root_folder}/valid.min.yaml')
+    
+def test_valid_assignee_email_config():
+    cm = ConfigManager(schema, f'{test_config_root_folder}/valid.assignee_email.yaml')
+    
+def test_invalid_assignee_email_config():
+    with pytest.raises(ConfigValidationError):
+        cm = ConfigManager(schema, f'{test_config_root_folder}/invalid.assignee_email.yaml')
+        
+def test_invalid_missing_option_config():
+    with pytest.raises(ConfigValidationError):
+        cm = ConfigManager(schema, f'{test_config_root_folder}/invalid.missing_option.yaml')
+
+def test_invalid_wrong_option_config():
+    with pytest.raises(ConfigValidationError):
+        cm = ConfigManager(schema, f'{test_config_root_folder}/invalid.wrong_option.yaml')
+        
